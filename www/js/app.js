8ecbfce006a44e858b56ec478b1095d9a9e89491
Andrew Magdy <andrewtawf@gmail.com>
2016-05-10 15:47:32 +0200

    Clean Up

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,3 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','pubnub.angular.service'])
 
 .run(function($ionicPlatform) {
@@ -60,25 +52,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','p
         }
       }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/chats');
